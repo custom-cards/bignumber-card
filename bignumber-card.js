3e4f21aeb487e56cad0745b0a318086bf5954916
Nito Buendia <effectedcard@gmail.com>
2020-05-09 14:42:16 +0800

    Allow default styling
    Create a new style property at config level. This property allows to configure a default colour for the graph when: (1) there is no severity defined, or (2) the severities do not match the current value.

--- bignumber-card.js
+++ bignumber-card.js
@@ -1,4 +1,6 @@
 class BigNumberCard extends HTMLElement {
+  _DEFAULT_STYLE = 'var(--label-badge-blue)';
+  
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
@@ -23,7 +25,7 @@ class BigNumberCard extends HTMLElement {
     style.textContent = `
       ha-card {
         text-align: center;
-        --bignumber-fill-color: var(--label-badge-blue);
+        --bignumber-fill-color: ${this._getStyle(null, cardConfig)};
         --bignumber-percent: 100%;
         --bignumber-direction: ${cardConfig.from};
         --base-unit: ${cardConfig.scale};
@@ -66,6 +68,7 @@ class BigNumberCard extends HTMLElement {
   }
 
   _computeSeverity(stateValue, sections) {
+    if (stateValue === undefined || stateValue === null) return;
     const numberValue = Number(stateValue);
     let style;
     sections.forEach(section => {
@@ -73,7 +76,16 @@ class BigNumberCard extends HTMLElement {
         style = section.style;
       }
     });
-    return style || 'var(--label-badge-blue)';
+    return style;
+  }
+
+  _getStyle(entityState, config) {
+    if (config.severity) {
+      const style = this._computeSeverity(entityState, config.severity);
+      if (style) return style;
+    }
+    if (config.style) return config.style;
+    return this._DEFAULT_STYLE;
   }
 
   _translatePercent(value, min, max) {
@@ -90,9 +102,7 @@ class BigNumberCard extends HTMLElement {
       if (config.min !== undefined && config.max !== undefined) {
         root.querySelector("ha-card").style.setProperty('--bignumber-percent', `${this._translatePercent(entityState, config.min, config.max)}%`);
       }
-      if (config.severity) {
-        root.querySelector("ha-card").style.setProperty('--bignumber-fill-color', `${this._computeSeverity(entityState, config.severity)}`);
-      }
+      root.querySelector("ha-card").style.setProperty('--bignumber-fill-color', `${this._getStyle(entityState, config)}`);
       root.getElementById("value").textContent = `${entityState} ${measurement}`;
       this._entityState = entityState
     }
